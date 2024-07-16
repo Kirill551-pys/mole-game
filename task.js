6540de6e3019a57380799a40e7e2c4f34fd72647
Kirill <taranyuk31@mail.ru>
2024-07-16 10:25:00 +0400

    change commit

--- task.js
+++ task.js
@@ -1,6 +1,8 @@
 let wins = 0;
 let losses = 0;
 let holes = [];
+const counterDead = document.getElementById(`dead`);
+const counterLost = document.getElementById(`lost`);
 
 function getHole(index) {
   return document.getElementById(`hole${index}`);
@@ -16,24 +18,12 @@ function getRandomHole() {
   return holes[Math.floor(Math.random() * holes.length)];
 }
 
-// Функция для добавления крота в лунку
-function addMole() {
-  const randomHole = getRandomHole();
-  randomHole.classList.add('hole_has-mole');
-  setTimeout(() => {
-    randomHole.classList.remove('hole_has-mole');
-    addMole();
-  }, 1000); // крот исчезает через 1 секунду
-}
-
-addMole();
-
 // Обработчик событий для каждой лунки
 holes.forEach((hole) => {
   hole.addEventListener('click', () => {
     if (hole.classList.contains('hole_has-mole')) {
       wins++;
-      console.log(`Wins: ${wins}`);
+      counterDead.textContent = `${wins}`;
       if (wins === 10) {
         alert('Вы победили!');
         wins = 0;
@@ -41,7 +31,7 @@ holes.forEach((hole) => {
       }
     } else {
       losses++;
-      console.log(`Losses: ${losses}`);
+      counterLost.textContent = `${losses}`;
       if (losses === 5) {
         alert('Вы проиграли!');
         wins = 0;
